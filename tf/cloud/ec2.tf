e83182d13bc05415d1b4b0832bb4ffdf33e9609c
Khiev Sothy <khiev.sothy@gmail.com>
2024-12-18 11:19:37 +0700

    v1

--- tf/cloud/ec2.tf
+++ tf/cloud/ec2.tf
@@ -25,9 +25,13 @@ resource "aws_security_group" "sg_1" {
   }
 }
 
-resource "aws_key_pair" "kkk3_key" {
-  key_name   = "kkk3-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCraET6lYDr5rSnxNH6eQODdBd4hIUjJ1pcyF/WDUft5jSR9iMq9PXJ5g6WdV7XNEOS9GK2liPqHiwglVZOTFjxno5Xf4CsNdqlsgM+paJplE9w4LR9yW/S8IzQaLjpMrvzQQTl+CffBfpxkezDmaU6IpTpf4Z9Yludx2A97IE57GUH/irY85+3uLlwM/CskFeBTwAek3vkgdtTjt0fLcuTIjfb/O2gr0opyz7wiiWJC3iN+nM1LS38D2gm2ji8BpZVhejap08zOIS0xwKSLfYmQIDjsiaQu5hGDMnO4ALZI8++p2E5VPgns2e7GNJYha6FNhD1ckijY4yCs1qpzY2F kimang@KIMs-MacBook-Pro.local"
+resource "aws_key_pair" "sothy_server_key" {
+  key_name   = "sothy-server-key"
+  public_key = "ssh-rsa b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACB7mYPLaKcqWyRpSIYTIKj2y4G5idtopNytglLc9bYECQAAAJg+IVaIPiFW
+iAAAAAtzc2gtZWQyNTUxOQAAACB7mYPLaKcqWyRpSIYTIKj2y4G5idtopNytglLc9bYECQ
+AAAEBcPmpldOkjq9DhPYOS7yEzjo1AxUsJxkb/vain2Zlnt3uZg8topypbJGlIhhMgqPbL
+gbmJ22ik3K2CUtz1tgQJAAAAFXNvdGh5QERFU0tUT1AtOUw5VEtGSw=="
 }
 
 data "aws_ami" "ubuntu" {
@@ -49,7 +53,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "server_1" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
-  key_name                    = aws_key_pair.kkk3_key.key_name
+  key_name                    = aws_key_pair.sothy_server_key.key_name
   security_groups             = [aws_security_group.sg_1.name]
   user_data                   = <<-EOF
               #!/bin/bash
@@ -68,7 +72,7 @@ resource "aws_instance" "server_1" {
               npm install pm2 -g
 
               # Clone Node.js repository
-              git clone https://github.com/KimangKhenng/devops-ex /root/devops-ex
+              git clone https://github.com/KhievSothy/devops-cadt-2-main /root/devops-ex
 
               # Navigate to the repository and start the app with PM2
               cd /root/devops-ex
